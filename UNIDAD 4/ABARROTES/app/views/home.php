bc6456d3081591c6d7c9525ecb1958f6b563c65a
kouta9 <kunkike07@gmail.com>
2023-11-13 12:07:09 -0600

    ini

---
+++ UNIDAD 4/ABARROTES/app/views/home.php
@@ -0,0 +1,17 @@
+<div class="container bg-danger text-light rounded bg-gradient p-5 mt-5">
+    <div class="row d-flex justify-content-center">
+        <div class="col text-center">
+            <h1>TIENDA DE ABARROTES KOUTA</h1>
+            <h3>Registro de productos</h3>
+            <hr>
+            <a href="./read" class="btn btn-info">Ir a Registrar Productos</a>
+        </div>
+    </div>
+    <div class="row my-5">
+        <div class="col-4"></div>
+        <div class="col-4 d-flex justify-content-center">
+            <img src="./public/img/img.jpeg" class="w-75 text-center" alt="">
+        </div>
+        <div class="col-4 "></div>
+    </div>
+</div>
